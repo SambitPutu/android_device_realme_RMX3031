a2f8244bce8d921477b42583f45f9abe511235ed
SUMIT <72486118+S8M1T@users.noreply.github.com>
2022-06-27 06:44:58 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX3031.mk
+    $(LOCAL_DIR)/voltage_RMX3031.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX3031-user \
-    lineage_RMX3031-userdebug \
-    lineage_RMX3031-eng
+    voltage_RMX3031-user \
+    voltage_RMX3031-userdebug \
+    voltage_RMX3031-eng
